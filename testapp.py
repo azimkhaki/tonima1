e977196bdc7786b580a3d027cbe27bd8e9666673
Azim Khaki <azimkhaki@Azims-MacBook-Pro.local>
2020-05-31 14:03:17 +0200

    Initial commit

---
+++ testapp.py
@@ -0,0 +1,14 @@
+character_name = "tonima"
+character_age = "50"
+is_male = False
+print ("there once was a lady called " + character_name + "," )
+print ("she was " + character_age + " years old")
+
+character_name = "azim"
+print ("she really liked the name " + character_name + "," )
+print ("but didnt like being" + character_age + ",")
+
+phrase = "school"
+print(phrase.index("h"))
+
+print (phrase.upper().isupper())
